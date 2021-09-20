3586f62dc70ca79dfc1779a47db046b4401d5f29
Jbril <jbrilh@gmail.com>
2021-09-20 18:37:13 +0300

    Setting up the ui and ecommerce shop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Navbar, Products} from './component'
+import {commerce} from './lib/commerce';
+
 
 const App = () => {
+
+    const [products, setproducts] = useState([]);
+    const fetchProducts = async() => {
+        const {data} = await commerce.products.list();
+        setproducts(data);
+        
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+    console.log(products);
     return (
         <div>
-            <h1>E-commerce</h1>
+            <Navbar />
+            <Products products={products} />
         </div>
     )
 }
