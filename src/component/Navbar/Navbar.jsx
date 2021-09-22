d1b2b1b09188d1a89d5ce370dd07adde657b774e
Jbril <jbrilh@gmail.com>
2021-09-22 16:03:57 +0300

    git adding useLocation hook

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,25 +2,34 @@ import React from 'react'
 import { AppBar, Badge, IconButton, Typography, Menu, MenuItem, Toolbar } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/commerce.png';
-import useStyles from './styles'
+import useStyles from './styles';
+import { Link, useLocation } from 'react-router-dom';
+
+
+
+
 const Navbar = ({totalItems}) => {
     const classes = useStyles();
+    const Location = useLocation();
+
     return (
         <>
           <AppBar position="fixed" className={classes.appbar} color="inherit">
               <Toolbar>
-                  <Typography variant="h6" className={classes.title} color="inherit">
+                  <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                       <img src={logo} alt="commerce.js" height="25px" className={classes.image} />
                       Commerce.js
                   </Typography>
                   <div className={classes.grow} />
+                  {Location.pathname === "/" && 
                   <div className={classes.button}>
-                      <IconButton aria-label="Show cart items" color="inherit">
+                      <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
                           <Badge badgeContent={totalItems} color="secondary">
                               <ShoppingCart />
                           </Badge>
                       </IconButton>
                   </div>
+                    }
               </Toolbar>
           </AppBar>
         </>
